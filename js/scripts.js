a3da63d1cabe6466f0b78b45aae07ba3d97e5b5a
Shanen Cross <shanencross@yahoo.com>
2021-02-04 01:07:22 -0800

    add up points for each language from question answers

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,59 @@ $("document").ready(function() {
 		event.preventDefault();
 		console.log("Submit pressed");
 
-		const result1 = $("input:radio[name=question1]:checked").val();
-		const result2 = $("input:radio[name=question2]:checked").val();
-		const result3 = $("input:radio[name=question3]:checked").val();
-		const result4 = $("input:radio[name=question4]:checked").val();
-		const result5 = $("input:radio[name=question5]:checked").val();
+		const challengeResult = $("input:radio[name=question1]:checked").val();
+		const gamesResult = $("input:radio[name=question2]:checked").val();
+		const websitesResult = $("input:radio[name=question3]:checked").val();
+		const frontOrBackResult = $("input:radio[name=question4]:checked").val();
+		const mathAndScienceResult = $("input:radio[name=question5]:checked").val();
 
-		console.log(result5);
+		let pythonPoints = 0;
+		let cSharpPoints = 0;
+		let jsPoints = 0;
+
+		if (challengeResult === "yes") {
+			cSharpPoints += 1;
+		} else if (challengeResult === "maybe") {
+			jsPoints += 1;
+		} else {
+			pythonPoints += 1;
+		}
+	
+		if (gamesResult === "unity") {
+			cSharpPoints += 1;
+		} else if (gamesResult === "HTML5") {
+			jsPoints += 1;
+		} else {
+			pythonPoints += 1;
+		}
+
+		if (websitesResult === "yes") {
+			jsPoints += 1;
+		} else if (websitesResult === "maybe") {
+			cSharpPoints += 1;
+		} else {
+			pythonPoints += 1;
+		}
+
+		if (frontOrBackResult === "front") {
+			jsPoints += 1;
+		} else if (frontOrBackResult === "back") {
+			cSharpPoints += 1;
+		} else {
+			pythonPoints += 1;
+		}
+
+		if (mathAndScienceResult === "yes") {
+			pythonPoints += 1;
+		} else if (mathAndScienceResult === "maybe") {
+			cSharpPoints += 1;
+		} else {
+			jsPoints += 1;
+		}
+
+		console.log("python points: " + pythonPoints);
+		console.log("cSharp points: " + cSharpPoints);
+		console.log("js points: " + jsPoints);
 
 		$("#questions").hide();
 		$("#result").show();
