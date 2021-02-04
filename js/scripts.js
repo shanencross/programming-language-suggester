8966e6eb8083369d59d0dc31f096ad8e22a4a24e
Shanen Cross <shanencross@yahoo.com>
2021-02-04 00:09:25 -0800

    test grabbing question results

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,14 @@ $("document").ready(function() {
 		event.preventDefault();
 		console.log("Submit pressed");
 
+		const result1 = $("input:radio[name=question1]:checked").val();
+		const result2 = $("input:radio[name=question2]:checked").val();
+		const result3 = $("input:radio[name=question3]:checked").val();
+		const result4 = $("input:radio[name=question4]:checked").val();
+		const result5 = $("input:radio[name=question5]:checked").val();
+
+		console.log(result5);
+
 		$("#questions").hide();
 		$("#result").show();
 	});
