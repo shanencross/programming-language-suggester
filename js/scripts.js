aa46c468943b6564b153a50c3d5c5c085316da6d
Shanen Cross <shanencross@yahoo.com>
2021-02-04 00:07:07 -0800

    show/hide result/questions upon clicking submit

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,8 @@ $("document").ready(function() {
 	$("form#questions").submit(function(event) {
 		event.preventDefault();
 		console.log("Submit pressed");
+
+		$("#questions").hide();
+		$("#result").show();
 	});
 });
